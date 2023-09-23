d1a8cbcdb91c26aba6cf46578c37249957fff7ac
Yves Martin des Taillades <59341242+yvesmartindestaillades@users.noreply.github.com>
2023-09-22 17:53:45 -0700

    Add files via upload

---
+++ dmsensei/models/templates.py
@@ -0,0 +1,130 @@
+import pytorch_lightning as pl
+from ..core import metrics
+from ..core.embeddings import NUM_BASES, sequence_to_one_hot, int_dot_bracket_to_one_hot
+import torch.nn as nn
+from torch import Tensor, tensor, zeros_like, mean
+import numpy as np
+from ..config import TEST_SETS_NAMES, UKN
+
+
+class Model(pl.LightningModule):
+    def __init__(self, lr: float, loss_fn, optimizer_fn, **kwargs):
+        super().__init__()
+
+        # Set attributes
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        self.lr = lr
+        self.loss_fn = loss_fn
+        self.optimizer_fn = optimizer_fn
+        self.automatic_optimization = True
+        self.save_hyperparameters()
+
+    def configure_optimizers(self):
+        return self.optimizer_fn(self.parameters(), lr=self.lr)
+
+
+class StructureModel(Model):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.data_type = "structure"
+
+    def validation_step(self, batch, batch_idx):
+        inputs, label = batch
+        outputs = self.forward(inputs)
+
+        # Compute loss
+        loss = self.loss_fn(outputs, label)
+        f1 = metrics.compute_f1(outputs, label, threshold=0.5)
+        mFMI = metrics.compute_mFMI(outputs, label, threshold=0.5)
+
+        # Logging to Wandb
+        self.log("valid/loss", loss)
+        self.log("valid/f1", f1)
+        self.log("valid/mFMI", mFMI)
+
+        return outputs, loss
+
+    def training_step(self, batch, batch_idx):
+        inputss, targets = batch
+        outputs = self.forward(inputss)
+
+        # Compute loss
+        loss = self.loss_fn(outputs, targets)
+
+        # Logging to TensorBoard
+        self.log("train/loss", loss)
+
+        return loss
+
+    def test_step(self, batch, batch_idx, dataloader_idx=0):
+        inputs, label = batch
+        outputs = self.forward(inputs)
+
+        # Compute and log loss
+        loss = self.loss_fn(outputs, label)
+        test_set_name = TEST_SETS_NAMES[self.data_type][dataloader_idx]
+        self.log(f"test/{test_set_name}", loss, add_dataloader_idx=False)
+
+        return outputs
+
+
+class DMSModel(Model):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.data_type = "dms"
+
+    def validation_step(self, batch, batch_idx):
+        inputs, label = batch
+        outputs = self.forward(inputs)
+
+        # Compute loss
+        loss = self.loss_fn(outputs, label)#[label != UKN], label[label != UKN])
+        r2 = mean(
+            tensor(
+                [
+                    metrics.r2_score(y_true, y_pred)
+                    for y_true, y_pred in zip(label, outputs)
+                ]
+            )
+        )
+
+        # Logging to Wandb
+        self.log("valid/loss", loss)
+        self.log("valid/r2", r2)
+
+        return outputs, loss
+
+    def training_step(self, batch, batch_idx):
+        #   sch = self.lr_schedulers()
+        #   sch.step()
+
+        inputs, label = batch
+
+        outputs = self.forward(inputs)
+
+        # Compute and log loss
+        loss = self.loss_fn(outputs, label)#[label != UKN], label[label != UKN])
+
+        # Logging to TensorBoard
+        self.log("train/loss", loss)
+
+        return loss
+
+    def test_step(self, batch, batch_idx, dataloader_idx=0):
+        inputs, label = batch
+        outputs = self.forward(inputs)
+
+        r2 = mean(
+            tensor(
+                [
+                    metrics.r2_score(y_true, y_pred)
+                    for y_true, y_pred in zip(label, outputs)
+                ]
+            )
+        )
+
+        test_set_name = TEST_SETS_NAMES[self.data_type][dataloader_idx]
+        self.log(f"test/{test_set_name}/r2", r2, add_dataloader_idx=False)
+
+        return outputs
