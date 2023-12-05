01592e4240c86b60a646dda8a62a482c5bdc1d6a
yvesmartindestaillades <yves.mrtn98@gmail.com>
2023-12-04 23:48:42 -0800

    init commit

--- dmsensei/core/datatype.py
+++ dmsensei/core/datatype.py
@@ -26,8 +26,29 @@ class DataTypeBatch(DataType):
 
 
 class DataTypeDataset(DataType):
-    def __init__(self, true, index, error=None):
+    def __init__(self, true, index, max_index, error=None):
+        self.max_index = max_index
         super().__init__(true=true, error=error, index=index)
+
+    def __len__(self):
+        return self.max_index
+
+    def __add__(self, other):
+        if self.name != other.name:
+            raise ValueError("Cannot add datasets of different types")
+
+        L = max(self.true.shape[1], other.true.shape[1])
+
+        return data_type_factory["dataset"][self.name](
+            true=torch.cat(
+                [_pad(self.true, L, self.name), _pad(other.true, L, self.name)], dim=0
+            ),
+            error=torch.cat(
+                [_pad(self.error, L, self.name), _pad(other.error, L, self.name)], dim=0
+            ),
+            index=torch.cat([self.index, other.index + self.max_index]),
+            max_index=self.max_index + other.max_index,
+        )
 
     @classmethod
     def from_data_json(cls, data_json: dict, L: int, refs: list):
@@ -64,6 +85,7 @@ class DataTypeDataset(DataType):
                 "true": post_process(true, torch.float32, dim=2),
                 "error": post_process(error, torch.float32, dim=2),
                 "index": post_process(index, torch.int32, dim=1),
+                "max_index": len(refs),
             }
         )
 
@@ -92,8 +114,8 @@ class StructureBatch(DataTypeBatch):
 class DMSDataset(DataTypeDataset):
     name = "dms"
 
-    def __init__(self, true, error=None, index=None):
-        super().__init__(true=true, error=error, index=index)
+    def __init__(self, true, max_index, error=None, index=None):
+        super().__init__(true=true, error=error, index=index, max_index=max_index)
 
     def __getitem__(self, idx):
         if idx not in self.index:
@@ -108,8 +130,8 @@ class DMSDataset(DataTypeDataset):
 class SHAPEDataset(DataTypeDataset):
     name = "shape"
 
-    def __init__(self, true, error=None, index=None):
-        super().__init__(true=true, error=error, index=index)
+    def __init__(self, true, max_index, error=None, index=None):
+        super().__init__(true=true, error=error, index=index, max_index=max_index)
 
     def __getitem__(self, idx):
         if idx not in self.index:
@@ -124,8 +146,8 @@ class SHAPEDataset(DataTypeDataset):
 class StructureDataset(DataTypeDataset):
     name = "structure"
 
-    def __init__(self, true, index=None, error=None):
-        super().__init__(true=true, index=index)
+    def __init__(self, true, max_index, error=None, index=None):
+        super().__init__(true=true, index=index, max_index=max_index)
 
     def __getitem__(self, idx):
         if idx not in self.index:
@@ -137,10 +159,7 @@ class StructureDataset(DataTypeDataset):
 
 
 data_type_factory = {
-    "batch": {
-        "dms": DMSBatch,
-        "shape": SHAPEBatch,
-        "structure": StructureBatch},
+    "batch": {"dms": DMSBatch, "shape": SHAPEBatch, "structure": StructureBatch},
     "dataset": {
         "dms": DMSDataset,
         "shape": SHAPEDataset,
